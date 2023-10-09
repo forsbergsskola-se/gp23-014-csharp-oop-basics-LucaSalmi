e959b08fabce64f8302e79c4723a40d06df75b00
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-10-09 13:57:00 +0200

    P1_1 completed

---
+++ P1_1Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+Person person = new Person();
+Animal animal = new Animal();
+Car car = new Car();
+
+Print(person);
+Print(animal);
+Print(car);
+
+void Print(Object toPrint)
+{
+    Console.WriteLine(toPrint);
+}
+
+public class Person{}
+public class Animal{}
+public class Car{}
+
