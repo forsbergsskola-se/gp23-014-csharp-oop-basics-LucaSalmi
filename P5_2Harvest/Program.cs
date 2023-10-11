a7ea8655fdf4242c1cac4663590581b893ee0e6e
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-10-11 13:59:48 +0200

    P5_2 harvest

---
+++ P5_2Harvest/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using P5_2Harvest;
+
+var plant = new Plant();
+var grain = new Grain();
+var wheat = new Wheat();
+
+plant.CanGrow();
+grain.CanGrow();
+grain.CanBeHarvested();
+wheat.CanGrow();
+wheat.CanBeHarvested();
+wheat.CanBeProcessedToBread();
